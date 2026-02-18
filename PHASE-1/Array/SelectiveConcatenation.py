58d179ab42999c03cf162bf2e5dcaecc78551773
Ayida-Aman <amanayida1@gmail.com>
2026-02-18 22:12:28 +0300

    speed up the teafication process

--- PHASE-1/Array/SelectiveConcatenation.py
+++ PHASE-1/Array/SelectiveConcatenation.py
@@ -3,54 +3,30 @@ for _ in range(t):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     b = []
+    
     if n == k:
         for i in range(len(a)):
             if (i+1) % 2 == 0:
                 b.append(a[i])
         b.append(0)
+        
+        res = -1
         for i in range(len(b)):
-            if i+1 != b[i]:
-                print(i+1)
+            if (i+1) != b[i]:
+                res = i+1
                 break
+        if res == -1:
+            res = (k // 2) + 1
+        print(res)
+    
     else:
-        size = n - (k// 2) 
-        if all(x == 1 for x in a[:size]): 
-            print(2) 
-        else: 
+        size = n - k + 2   
+        isOne = True
+        for i in range(1, size):  
+            if a[i] != 1:
+                isOne = False
+                break
+        if isOne:
+            print(2)
+        else:
             print(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # size = n//k
-    # b = [a[i*size:(i+1)*size] for i in range(k-1)] 
-    # b.append(a[(k-1)*size:])
-    # print(b)
-    # res = []
-    # for i in range(len(b)):
-    #     if (i+1) %2 == 0:
-    #         for j in range(len(b[i])):
-    #             res.append(b[i][j])
-    # res.append(0)
-    # print(res)
-    # for i in range(len(res)):
-    #     if i+1 != res[i]:
-    #         print(i+1)
-    #         break
-            
